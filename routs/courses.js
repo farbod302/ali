3f4ffdaee08d4e8675d728e0abc957c622ddeaef
farbod302 <f.aliakbari302@gmail.com>
2022-08-18 10:59:06 +0430

    mian

---
+++ routs/courses.js
@@ -0,0 +1,56 @@
+const express = require('express')
+const Courses = require('../db/courses')
+const router = express.Router()
+
+router.post("/add", (req, res) => {
+    const new_course = { ...req.body }
+    new Courses(new_course).save()
+    res.json({
+        status: true,
+        msg: "دوره اضافه دش",
+        data: {}
+    })
+})
+
+router.post("/edit", async (req, res) => {
+    const { id } = req.body
+    let keys = Object.keys(req.body)
+    let exist_course = await Courses.findById(id)
+    if (!exist_course) {
+        res.json({
+            status: false,
+            msg: "دوره نامعتبر",
+            data: {}
+        })
+        return
+    }
+
+    let query = {}
+    keys.forEach(key => {
+        query[key] = req.body[key]
+    })
+    delete query.token
+    delete query.id
+
+    await Courses.findByIdAndUpdate(id, { $set: query })
+    res.json({
+        status: true,
+        msg: "دوره ویرایش شد",
+        data: {}
+    })
+})
+
+router.post("/add_videos",async (req, res) => {
+    await Courses.findByIdAndUpdate(req.body.id, { $push: { videos: { ...req.body } } })
+    res.json({
+        status: true,
+        msg: "ویدئو اضافه شد",
+        data: {}
+    })
+
+})
+
+
+
+
+module.exports = router
